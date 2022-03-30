af901fb0470c83a4dddd175bfc77ade9f01fc62a
Imran Ahmed <ahmed.imran01@ou.edu>
2022-03-30 17:40:14 -0500

    Worked on some API calls

---
+++ FreeAPIs/BoredAPI/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BoredAPI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            //List<TheBoredAPI> information = new List<TheBoredAPI>();
+            List<TheBoredAPI> apiInfo;
+
+            string url = "http://www.boredapi.com/api/activity/";
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(url).Result;
+
+                TheBoredAPI api = JsonConvert.DeserializeObject<TheBoredAPI>(json);
+
+            }
+        }
+    }
+}
---
+++ FreeAPIs/BoredAPI/TheBoredAPI.cs
@@ -0,0 +1,14 @@
+﻿namespace BoredAPI
+{
+    public class TheBoredAPI
+    {
+        public string activity { get; set; }
+        public string type { get; set; }
+        public int participants { get; set; }
+        public double price { get; set; }
+        public string link { get; set; }
+        public string key { get; set; }
+        public double accessibility { get; set; }
+
+    }
+}
